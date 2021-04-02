a704ecd2914dd3d6634b6fdaeb4d017bd9050fc3
PaulaCBS <paulasalvado2019@gmail.com>
2021-04-02 15:44:06 -0300

    add estrutura inicial

---
+++ src/components/shared/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    &copy; ArcH
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Users.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Usuários
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Users'
+}
+</script>
+
+<style scoped>
+
+</style>
